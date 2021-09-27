3ede3ac6373cafc94d9000bfefe0c1e381a4e92b
pescaru98 <mihai.pescaru@elysian-software.com>
2021-09-27 09:38:02 +0200

    Added product service

--- src/app/components/products/product-list.component.ts
+++ src/app/components/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from "../../models/Product/product";
+import {ProductService} from "../../services/product/product.service";
 
 
 @Component({
@@ -27,42 +28,14 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value);
   }
 
-  products: IProduct[] = [{
-    "productId": 1,
-    "productName": "Dell Laptop",
-    "productCode": "GGF-423-DD",
-    "releaseDate": "19-02-2021",
-    "description": "Gaming Laptop",
-    "price": 2000,
-    "starRating": 4.3,
-    "imageUrl": "assets/images/default-logo.png"
-  },
-    {
-      "productId": 2,
-      "productName": "Lenovo Monitor",
-      "productCode": "TRZ-55-YY",
-      "releaseDate": "04-05-2021",
-      "description": "144Hz Monitor",
-      "price": 600,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/default-logo.png"
-    },
-    {
-      "productId": 3,
-      "productName": "Lenovo Gaming PC",
-      "productCode": "PPI-612-LE",
-      "releaseDate": "24-10-2020",
-      "description": "Best deal on PCs",
-      "price": 2330,
-      "starRating": 4.7,
-      "imageUrl": "assets/images/default-logo.png"
-    }];
+  products: IProduct[] = [];
 
-  constructor() {
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit(): void {
-    this.listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   toggleImage() {
